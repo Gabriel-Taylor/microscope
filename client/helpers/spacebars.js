4605255b1a849203b404347141ea049b1a7a3268
Gabriel <gabriel@grout.io>
2014-11-07 11:38:13 +1100

    13.3 completed

---
+++ client/helpers/spacebars.js
@@ -0,0 +1,8 @@
+UI.registerHerlper('pluralise', function(n, thing) {
+    // fairly stupid pluralizer
+    if (n === 1) {
+        return '1 ' + thing;
+    } else {
+        return n + ' ' + thing + 's';
+    }
+});
